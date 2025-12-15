667a2848ac8b2cef82393425eb08cc50e8f5059b
Vitaliy Kamelin <v.kamelin@gmail.com>
2025-12-15 11:22:22 +0500

    feat: добавлены вспомогательные методы и уточнены возвращаемые типы в модель Section

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Collection as BaseCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -69,9 +69,39 @@ class Section extends Model
     /**
      * Получение дочерних разделов
      */
-    public function getChildren(): Collection
+    public function getChildren(): BaseCollection
     {
         return $this->children()->get();
+    }
+
+    public function getParent(): ?Section
+    {
+        return $this->parent()->first();
+    }
+
+    public function getCollection(): Collection
+    {
+        return $this->collection()->first();
+    }
+
+    public function isActive(): bool
+    {
+        return ($this->is_active ?? false);
+    }
+
+    public function isPublished(): bool
+    {
+        return $this->attributes['status'] === 'published';
+    }
+
+    public function isHidden(): bool
+    {
+        return $this->attributes['status'] === 'hidden';
+    }
+
+    public function isArchived(): bool
+    {
+        return $this->attributes['status'] === 'archived';
     }
 
     protected function casts(): array
