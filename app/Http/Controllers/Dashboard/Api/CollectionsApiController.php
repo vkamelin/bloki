11a22686e33b94571f34ec933b5feb659a41f0f4
Vitaliy Kamelin <v.kamelin@gmail.com>
2025-12-24 14:30:49 +0500

    feat: Добавил методы API для коллекций

--- app/Http/Controllers/Dashboard/Api/CollectionsApiController.php
+++ app/Http/Controllers/Dashboard/Api/CollectionsApiController.php
@@ -3,26 +3,62 @@
 namespace App\Http\Controllers\Dashboard\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CollectionRequest;
+use App\Models\Collection;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class CollectionsApiController extends Controller
 {
-    public function index()
+    public function index(Request $request): JsonResponse
     {
-        // Return JSON response for collections table
+        // TODO: Реализовать получение списка коллекций с пагинацией, сортировкой и фильтрацией
+
+        return response()->json([], 200);
     }
 
-    public function store()
+    public function store(CollectionRequest $request): JsonResponse
     {
-        // Handle collection creation
+        // TODO: Реализовать создание новой коллекции
+
+        return response()->json([], 200);
     }
 
-    public function update($id)
+    public function show(Collection $collection): JsonResponse
     {
-        // Handle collection update
+        // TODO: Реализовать получение деталей коллекции
+
+        return response()->json([], 200);
     }
 
-    public function destroy($id)
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CollectionRequest $request, Collection $collection): JsonResponse
     {
-        // Handle collection deletion
+        // TODO: Реализовать обновление коллекции
+
+        return response()->json([], 200);
+    }
+
+    public function destroy(Collection $collection): JsonResponse
+    {
+        // TODO: Реализовать удаление коллекции
+
+        return response()->json([], 200);
+    }
+
+    public function restore(Collection $collection): JsonResponse
+    {
+        // TODO: Реализовать восстановление удаленной коллекции
+
+        return response()->json([], 200);
+    }
+
+    public function duplicate(Collection $collection): JsonResponse
+    {
+        // TODO: Реализовать создание копии коллекции
+
+        return response()->json([], 200);
     }
 }
