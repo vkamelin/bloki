b84492aa81bc2ef777e899d8be80d2a7752b26d8
Vitaliy Kamelin <v.kamelin@gmail.com>
2025-12-26 12:32:40 +0500

    feat: добавлена валидация для slug в FieldRequest

--- app/Http/Requests/FieldRequest.php
+++ app/Http/Requests/FieldRequest.php
@@ -8,7 +8,7 @@ use App\Services\Dashboard\FieldConfiguration;
 class FieldRequest extends FormRequest
 {
     protected $fieldConfig;
-    
+
     public function __construct()
     {
         parent::__construct();
@@ -20,7 +20,8 @@ class FieldRequest extends FormRequest
         return [
             'group_id' => ['required', 'exists:field_groups'],
             'uuid' => ['required'],
-            'name' => ['required'],
+            'slug' => ['required', 'string', 'max:100', 'unique:fields,slug'],
+            'name' => ['required', 'string', 'max:100'],
             'description' => ['nullable'],
             'instructions' => ['nullable'],
             'type' => ['required', 'in:' . implode(',', $this->fieldConfig->getFieldTypeNames())],
