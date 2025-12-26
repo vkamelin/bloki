a457a866e8aaa0a550c2829e6f79fbb3a2a5a475
Vitaliy Kamelin <v.kamelin@gmail.com>
2025-12-26 12:07:05 +0500

    feat: добавлено больше методов в CollectionRepositoryInterface

--- app/Repositories/Interfaces/CollectionRepositoryInterface.php
+++ app/Repositories/Interfaces/CollectionRepositoryInterface.php
@@ -2,25 +2,42 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Models\Collection;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
 interface CollectionRepositoryInterface
 {
-    public function create(array $data);
+    public function create(array $data): ?Collection;
 
-    public function update(int $id, array $data);
+    public function update(int $id, array $data): ?Collection;
 
-    public function delete(int $id);
+    public function delete(int $id): bool;
 
-    public function restore(int $id);
+    public function restore(int $id): ?Collection;
 
-    public function getAll(array $filters = []);
+    public function getAll(array $filters = []): array;
 
-    public function getById(int $id);
+    public function getById(int $id): ?Collection;
 
-    public function getBySlug(string $slug);
+    public function getBySlug(string $slug): ?Collection;
 
-    public function paginate(int $perPage = 15);
+    public function paginate(int $perPage = 15, array $filters = []): LengthAwarePaginator;
 
-    public function where(array $conditions);
+    public function where(array $conditions): array;
 
-    public function with(array $relations);
+    public function with(array $relations): array;
+
+    public function duplicate(int $id): ?Collection;
+
+    public function getSingleton(string $handle): ?Collection;
+
+    public function isSingleton(int|Collection $collection): bool;
+
+    public function hasSections(int|Collection $collection): bool;
+
+    public function getSectionStructure(int|Collection $collection): array;
+
+    public function getDefaultTemplateSection(int|Collection $collection): ?string;
+
+    public function getDefaultTemplateEntry(int|Collection $collection): ?string;
 }
